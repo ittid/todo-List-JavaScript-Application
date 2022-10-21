540dcfb9c07af0ac5f2700916b9f0db3a67b329a
ittid <ahnaine3bd@gmail.com>
2022-10-21 12:46:29 +0100

    first version

---
+++ index.js
@@ -0,0 +1,34 @@
+/*=== === === === === === === === ===
+~~~~ JavaScript ToDo List Code ~~~~
+=== === === === === === === === ===*/
+const form = document.getElementById("form");
+const input = document.querySelector("[data-input]");
+const submitBtn = document.querySelector("[data-submit]");
+const clearAll = document.querySelector("[data-clear]");
+const output = document.getElementsByClassName("task")[0];
+
+addEventListener("DOMContentLoaded", function () {
+  // Prevent Default
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const template = `
+    <label class="container">
+      ${input.value}
+      <input type="checkbox" />
+      <span class="checkmark"></span>
+    </label>
+  `;
+    output.innerHTML += template;
+  });
+
+  submitBtn.addEventListener("click", function () {
+    setTimeout(function () {
+      input.value = "";
+    }, 500);
+  });
+
+  clearAll.addEventListener("click", function () {
+    confirm("Do You Want To Clear All This Taskes.?");
+    output.innerHTML = "";
+  });
+});
