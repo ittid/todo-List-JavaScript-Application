fa51ba2a79c6cd724e2a9bf269e218f0710a1c07
ittid <ahnaine3bd@gmail.com>
2022-11-03 23:05:31 +0100

    fix bugs and make the html code clean and validate javascript

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /*=== === === === === === === === ===
 ~~~~ JavaScript ToDo List Code ~~~~
 === === === === === === === === ===*/
+
+// Declare Variables :
 const form = document.getElementById("form");
 const input = document.querySelector("[data-input]");
 const submitBtn = document.querySelector("[data-submit]");
@@ -8,8 +10,8 @@ const clearAll = document.querySelector("[data-clear]");
 const output = document.getElementsByClassName("task")[0];
 
 addEventListener("DOMContentLoaded", function () {
-  // Prevent Default
   form.addEventListener("submit", function (e) {
+    // Prevent Default
     e.preventDefault();
     const template = `
     <label class="container">
@@ -25,10 +27,21 @@ addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
       input.value = "";
     }, 500);
+    clearAll.innerText = "Clear All";
   });
 
-  clearAll.addEventListener("click", function () {
-    confirm("You are goin to clear all the tasks.?");
-    output.innerHTML = "";
+  clearAll.addEventListener("click", function (e) {
+    /* fix some bugs here when the task is empty we show a msg and prevent default */
+    if (document.getElementsByClassName("container").length > 0) {
+      confirm("You are goin to clear all the tasks.?");
+      output.innerHTML = "";
+    }
+    if (document.getElementsByClassName("container").length == 0) {
+      this.innerText = "its already empty";
+    }
+  });
+
+  document.getElementById("checkmark").addEventListener("click", () => {
+    alert("congratulation you fishe your task");
   });
 });
